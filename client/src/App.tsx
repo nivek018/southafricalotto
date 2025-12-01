b011df1910019c6be8ace316464236541b829065
nivek018 <kevs022@gmail.com>
2025-12-01 10:34:48 +0800

    add to homepage recent lotto result section

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import { Switch, Route, useLocation } from "wouter";
+import { useEffect } from "react";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -53,7 +54,12 @@ function Router() {
 function AppLayout() {
   const [location] = useLocation();
   const isAdminRoute = location.startsWith("/encode");
-  const isHome = location === "/";
+
+  useEffect(() => {
+    if (!isAdminRoute) {
+      window.scrollTo({ top: 0, behavior: "instant" as ScrollBehavior });
+    }
+  }, [location, isAdminRoute]);
 
   if (isAdminRoute) {
     return <Router />;
@@ -65,7 +71,7 @@ function AppLayout() {
       <main className="flex-1">
         <Router />
       </main>
-      {!isHome && <RecentResultsSection />}
+      <RecentResultsSection />
       <Footer />
     </div>
   );
